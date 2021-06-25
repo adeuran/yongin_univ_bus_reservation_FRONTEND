df14dab7e12534fa161025254651168ccc37b105
HyeonbeomHeo <contact@snowytiger.me>
2021-06-26 00:39:40 +0900

    add stationlist

---
+++ src/templates/admin/StationList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <top-header></top-header>
+  </div>
+</template>
+
+<script>
+import TopHeader from "@/organisms/TopHeader.vue"
+
+export default {
+  components: {
+    TopHeader,
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
